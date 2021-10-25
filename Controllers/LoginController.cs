ae6b677572b3aca3b3d233c29254aae4b32029ef
STRIKEFORCE01 <93120381+STRIKEFORCE01@users.noreply.github.com>
2021-10-25 14:21:06 +0600

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLoginApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterAndLoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if(userModel.UserName == "Boah" && userModel.Password == "1899")
+            {
+                return View("LoginSuccess", userModel);
+
+            }else
+            {
+                return View("LoginFailure", userModel);
+            }
+           
+        }
+    }
+}
